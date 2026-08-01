ce36b29cc65b73cecabeb0ec308d59b016cae646
Winner <blessedwinner66@gmail.com>
2026-08-01 11:56:21 +0200

    Allow farmer role choice in backend RegisterSerializer

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -145,7 +145,7 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    role = serializers.ChoiceField(choices=[('client', 'Client')], default='client')
+    role = serializers.ChoiceField(choices=[('client', 'Client'), ('farmer', 'Farmer')], default='client')
     business_name = serializers.CharField(required=False, allow_blank=True)
     farm_name = serializers.CharField(required=False, allow_blank=True)
     delivery_address = serializers.CharField(required=False, allow_blank=True)
